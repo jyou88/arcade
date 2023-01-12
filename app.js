7d09425db8033b8d2610a5e85f9bf7035100a591
Jae You <jyou88@gmail.com>
2023-01-12 06:15:31 -0600

    "Update 2"

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-let boardback = document.querySelector(".boardback");
-let gameboard = document.querySelector(".board");
-let count = 0
-let restartButton = document.getElementById('restartButton');
-const gameState = {
-    players: ['X', 'O'],
-    board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ]
-}
-let winningMessage = document.getElementById('winningMessage')
+const tiles = Array.from(document.querySelectorAll('.tile'));
+const playerDisplay = document.querySelector('.display-player');
+const resetButton = document.querySelector('#reset');
+const announcer = document.querySelector('.announcer');
 
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let isGameActive = true;
 
-const winningCombos = [
+const PLAYERX_WON = 'PLAYERX_WON';
+const PLAYERO_WON = 'PLAYERO_WON';
+const TIE = 'TIE';
+
+const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -22,63 +20,95 @@ const winningCombos = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+];
 
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        const a = board[winCondition[0]];
+        const b = board[winCondition[1]];
+        const c = board[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
 
+    if (roundWon) {
+        announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+        isGameActive = false;
+        return;
+    }
 
-function playGame(event) {
-    if (event.target.innerHTML === "") {
-        event.target.innerHTML = count % 2 === 0 ? "X" : "O"
-        count++
+    if (!board.includes(''))
+        announce(TIE);
+}
+
+const announce = (type) => {
+    switch (type) {
+        case PLAYERO_WON:
+            announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+            break;
+        case PLAYERX_WON:
+            announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+            break;
+        case TIE:
+            announcer.innerText = 'Tie';
+    }
+    announcer.classList.remove('hide');
+};
+
+const isValidAction = (tile) => {
+    if (tile.innerText === 'X' || tile.innerText === 'O') {
+        return false;
+    }
+
+    return true;
+};
+
+const updateBoard = (index) => {
+    board[index] = currentPlayer;
+}
+
+const changePlayer = () => {
+    playerDisplay.classList.remove(`player${currentPlayer}`);
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    playerDisplay.innerText = currentPlayer;
+    playerDisplay.classList.add(`player${currentPlayer}`);
+}
+
+const userAction = (tile, index) => {
+    if (isValidAction(tile) && isGameActive) {
+        tile.innerText = currentPlayer;
+        tile.classList.add(`player${currentPlayer}`);
+        updateBoard(index);
+        handleResultValidation();
+        changePlayer();
     }
 }
 
+const resetBoard = () => {
+    board = ['', '', '', '', '', '', '', '', ''];
+    isGameActive = true;
+    announcer.classList.add('hide');
 
-function restartGame() {
-    gameState = {
-        players: ['X', 'O'],
-        board: [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null]
-        ]
+    if (currentPlayer === 'O') {
+        changePlayer();
     }
-    count = 0;
-    render()
 
+    tiles.forEach(tile => {
+        tile.innerText = '';
+        tile.classList.remove('playerX');
+        tile.classList.remove('playerO');
+    });
 }
 
-gameboard.addEventListener("click", playGame);
+tiles.forEach((tile, index) => {
+    tile.addEventListener('click', () => userAction(tile, index));
+});
 
-restartButton.addEventListener("click", restartGame);
-
-// let tic = document.addEventListener("click", (event) => { })
-
-// boardback.addEventListener("click", () => { })
-// console.log(matrix[0][0]);
-
-// let matrix = [
-
-//     ["00", "01", "02"],
-//     ["10", "11", "12"],
-//     ["20", "21", "22"],
-// ]
-
-    // setInterval(() => {
-    //     const arr = ["red", "blue", "yellow", "green"];
-
-    //     let choose = arr[Math.floor(Math.random() * arr.lenth)];
-
-    //     boardback.getElementsByClassName.background = choose;
-    // }, 200);
-
-
-    // const gameState = {
-//     players: ['X', 'O'],
-//     board: [
-//         [null, null, null],
-//         [null, null, null],
-//         [null, null, null]
-//     ]
-// }
-// gameState()
+restartButton.addEventListener('click', resetBoard);
